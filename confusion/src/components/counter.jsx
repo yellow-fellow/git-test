e03a1a4d544148151d22d7b97e34910fdd544966
yellow-fellow <huangshaohang.98@gmail.com>
2021-04-25 14:31:22 +0800

    Added padding and improved on styling

--- confusion/src/components/counter.jsx
+++ confusion/src/components/counter.jsx
@@ -31,6 +31,8 @@ class Counter extends Component {
                 return parseFloat(this.state.storedValue_1) * parseFloat(this.state.storedValue_2);
             case '/':
                 return parseFloat(this.state.storedValue_1) / parseFloat(this.state.storedValue_2);
+            case '&#8730;':
+                return Math.sqrt(parseFloat(this.state.storedValue_1));
             default:
                 return "result";
         }
@@ -41,18 +43,18 @@ class Counter extends Component {
             <div align='center'>
                 {this.props.children}
                 <h3><span className = "badge badge-primary" style= {{backgroundColor: '#282c34'}}>{this.state.value}</span></h3>
-                <InputGroup>
+                <InputGroup style = {{paddingInline: "50px"}}>
                     {/* <InputGroupAddon addonType = "prepend">
                         <InputGroupText>Value:</InputGroupText>
                     </InputGroupAddon> */}
-                    <Input placeholder="Input 1st value here" className = 'm-2' onChange = {(e) => {this.setState( {storedValue: this.state.storedValue_1 = e.target.value} )}}></Input>                    
+                    <Input placeholder="Input 1st value here" className = 'm-2' style = {{border: '2px solid'}} onChange = {(e) => {this.setState( {storedValue: this.state.storedValue_1 = e.target.value} )}}></Input>                    
                 </InputGroup>
-                <InputGroup>
-                    <Input placeholder="Input 2nd value here" className = 'm-2' onChange = {(e) => {this.setState( {storedValue: this.state.storedValue_2 = e.target.value} )}}></Input>
+                <InputGroup style = {{paddingInline: "50px"}}>
+                    <Input placeholder="Input 2nd value here" className = 'm-2' style = {{border: '2px solid'}} onChange = {(e) => {this.setState( {storedValue: this.state.storedValue_2 = e.target.value} )}}></Input>
                 </InputGroup>
                 <br></br>
                 <div>
-                    {this.state.storedValue_1} {this.state.currentState} {this.state.storedValue_2} = {this.chooseOperator()}
+                    {(this.state.storedValue_1 === 0) ? "": this.state.storedValue_1} {(this.state.currentState === "operator") ? "": this.state.currentState} {(this.state.storedValue_2 === 0) ? "": this.state.storedValue_2} {((this.state.storedValue_1 === 0) && (this.state.storedValue_2 === 0)) ? "": "="} {(this.chooseOperator() === "result") ? "": this.chooseOperator()}
                 </div>
                 <br></br>
                 <button className = "btn btn-outline-primary m-4" onClick = {() => {this.setState( {currentState: this.state.currentState = "+"} )}}>+</button>
